7d6fad7f96a4d2e59a7e19582d76f1cd2f312a2d
kiranrathor30697 <kiranrathor30697@gmail.com>
2021-10-07 23:32:02 +0530

    word by word paragraph

--- js/function.js
+++ js/function.js
@@ -57,8 +57,12 @@ function functionD(a2a,a3a,a4a,a5a){//formal argument/parameter
 let u = functionD(8,45,234,functionD(55,59,85,57));//actual argument/parameter
 console.log(u);
 
+var para = " ";
 var result = ['Health',' ministry,','flags','five','states','for','high','number','of','active','Covid-19','cases'];
-console.log(result);
+
+para = para + result + "  ";
+console.log(para);
+
 
 var result1 = [...result, 'india','news'];
 console.log(result1);
@@ -66,7 +70,7 @@ console.log(result1);
 var z = ['kiran','Kamlesh','Rudransh','Anjali','Sarita','Harish'];
 var z2 =[...z,'Vijay','krishna','Laddu'];
 var k   = [...result,...result1,...z,...z2,'kavita'];
-console.log(k);
+console.log(k.length);
 
 //5.function definition
 function functionE(result1){//formal argument/parameter
@@ -84,7 +88,7 @@ var b2 = [...b1,'six','magnitude']
 var b3 = [...b2,'shakes','Tokyo'];
 var b4 = [...b3,'area;'];
 var b5 = [...b4,'no','tsunami','danger'];
-console.log(b5);
+console.log(b5.length);
 
 var paragraph = '';
 //(initialization;condition,increment/decrement)
@@ -103,3 +107,28 @@ let functionF = (name)=>{//formal argument/parameter
 }
 //6.function calling
 functionF("Anjali");//actual argument/parameter
+
+
+//7.function definition
+var h1 = ["SURAT:","The","Tapi","river","through","the","Diamond","City","will","be","flowing","bank-to-bank","from","Monday","late","evening","with","the","release","of",1.75,"lakh","cusecs","water","from","the","Ukai","dam."];
+var h2 = [...h1,"The","amount","of","water","released","from","the","Ukai","dam","was","increased","from",1.5,"lakh","cusecs","following","heavy","inflow","from","the","Tapi","catchment","area","which","witnessed","incessant","rain","under","the","influence","of","cyclone","Gluab","in the Bay of"," Bengal."];
+var h3 = [...h2,"The","water","level","in","the","Tapi","river","has","been","on","the","rise","since","Monday","afternoon",",said","city","authorities."];
+var h4 = [...h3,"Out","of","the","total","water","being","released","from","Ukai","dam,",1.51,"lakh","cusecs","was","released","through","gates","while"];
+var h5 = [...h4,22,512,"cusecs","was","being","released","from","the","four","hydropower"," units.","Water","level","in","the","dam","that","was",342.29,"ft"];
+var h6 = [...h5,"on","Monday","morning","was","brought","down","to",341.91,"ft.","Ukai","dam","authorities","further","plan","to","increase","the","water"];
+var h7 = [...h6,"released","to",1.95,"lakh","cusecs","considering","the","inflow."];
+
+let functionG = (news)=>{//formal argument/parameter
+    //function body
+    //(initialization;condition;increment/decrement)
+    var paragraph1 = " ";
+    for(var i=0;i<h7.length;i++){
+        //console.log(news + h7[i]+" ");
+        paragraph1 = paragraph1 +h7[i];
+    }
+    console.log(paragraph1);
+}
+//7.function calling
+functionG("NEWS");
+
+
